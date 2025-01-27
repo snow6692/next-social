5b4da05b98da37c99f7b4ba8106120c81f700e2a
snow6692 <ahmedha258258@gmail.com>
2025-01-27 11:17:52 +0200

    Connect prisma with clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-
-
-export default function Home() {
+export default async function Home() {
   return <div className="m-4">home page</div>;
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,13 @@ import Link from "next/link";
 import React from "react";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/userAction";
 
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser();
+
+  if (user) await syncUser();
   return (
     <nav className="sticky top-0 z-50 w-full border-b to-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-auto max-w-7xl px-4">
